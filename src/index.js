96b5c364d3c60412c2abd773da25504bf7ffb31e
MACTEPyc <MACTEPyc@gmail.com>
2021-08-31 01:00:59 +0500

    feat: edit function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN = bracketsConfig.map(item => item[0]);
+  const PAIR = bracketsConfig.reduce((obj, item) => {
+    obj[item[1]] = item[0];
+    return obj;
+  }, {});
+  let stack = [];
+  //console.log(str.split('||'));
+  for (let symb of str) {
+    if (OPEN.includes(symb)) { 
+      stack.push(symb);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topElement = stack[stack.length - 1];
+      //console.log(symb);
+      if (PAIR[symb] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      } 
+    }
+    //console.log({stack});
+  }
+  return stack.length === 0;
 }
+/*
+console.log(check('()', [['(', ')']]));
+console.log(check('((()))()', [['(', ')']]));
+console.log(check('())(', [['(', ')']]));
+console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
+console.log(check('[(])', [['(', ')'], ['[', ']']]));
+console.log(check('[]()', [['(', ')'], ['[', ']']]));
+console.log(check('[]()(', [['(', ')'], ['[', ']']]));
+console.log(check('((()))()', [['(', ')']]));
+console.log(check('||', [['|', '|']]));
+console.log(check('|()|', [['(', ')'], ['|', '|']]));
+console.log(check('|(|)', [['(', ')'], ['|', '|']]));
+console.log(check('|()|(||)||', [['(', ')'], ['|', '|']]));
+*/
